fd1dd6381242bf96ec9b50d390df49ec99a00c27
APSCoding13 <51973395+AnshS13@users.noreply.github.com>
2026-09-17 16:44:05 -0700

    Added the teleop code from 9/15/26 and then i cloned the repsotiory from the BIo Buzz SDK

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPCode.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp(name = "Testing2")
+public class TeleOPCode extends LinearOpMode {
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+
+    @Override
+    public void runOpMode() {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+//        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        waitForStart();
+        telemetry.addData("Telemetry", "Called");
+        while (opModeIsActive()) {
+            telemetry.addData("X: ", gamepad1.left_stick_x);
+            telemetry.addData("Y: ", gamepad1.left_stick_y);
+            telemetry.update();
+
+            if (Math.abs(gamepad1.left_stick_y) > 0.25  || Math.abs(gamepad1.left_stick_x) >0.25) {
+                double vector_Add =  (-1 * gamepad1.left_stick_y) + gamepad1.left_stick_x;
+                double vector_Sub =  (-1 * gamepad1.left_stick_y) - gamepad1.left_stick_x;
+                int negative_Add = vector_Add<0?-1:1;
+                int negative_sub = vector_Sub<0?-1:1;
+
+
+                frontLeft.setPower(Math.abs(vector_Add)>1? negative_Add : vector_Add);
+                frontRight.setPower(Math.abs(vector_Sub)>1? negative_sub : vector_Sub);
+                backLeft.setPower(Math.abs(vector_Sub)>1? negative_sub : vector_Sub);
+                backRight.setPower(Math.abs(vector_Add)>1? negative_Add : vector_Add);
+            }
+            else{
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backLeft.setPower(0);
+                backRight.setPower(0);
+            }
+        }
+    }
+}
+
