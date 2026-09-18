0976dcd4efdf69ea29fbc41535f6e6001a40f040
APSCoding13 <51973395+AnshS13@users.noreply.github.com>
2026-09-17 17:19:44 -0700

    Optimized omni directional movement by collapsing calculations into variables to remove redundant calculations. Theoretically added rotation in both clockwise and counter clockwise, but needs to be tested first.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPCode.java
@@ -23,29 +23,58 @@ public class TeleOPCode extends LinearOpMode {
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         waitForStart();
         telemetry.addData("Telemetry", "Called");
+
+
+
         while (opModeIsActive()) {
             telemetry.addData("X: ", gamepad1.left_stick_x);
             telemetry.addData("Y: ", gamepad1.left_stick_y);
             telemetry.update();
 
-            if (Math.abs(gamepad1.left_stick_y) > 0.25  || Math.abs(gamepad1.left_stick_x) >0.25) {
-                double vector_Add =  (-1 * gamepad1.left_stick_y) + gamepad1.left_stick_x;
-                double vector_Sub =  (-1 * gamepad1.left_stick_y) - gamepad1.left_stick_x;
-                int negative_Add = vector_Add<0?-1:1;
-                int negative_sub = vector_Sub<0?-1:1;
+            Omnimovement();
+        }
 
 
-                frontLeft.setPower(Math.abs(vector_Add)>1? negative_Add : vector_Add);
-                frontRight.setPower(Math.abs(vector_Sub)>1? negative_sub : vector_Sub);
-                backLeft.setPower(Math.abs(vector_Sub)>1? negative_sub : vector_Sub);
-                backRight.setPower(Math.abs(vector_Add)>1? negative_Add : vector_Add);
+    }
+    public void Omnimovement()
+    {
+        //Uses a Y-Vector and X-Vector, adds them to get
+        if (Math.abs(gamepad1.left_stick_y) > 0.25  || Math.abs(gamepad1.left_stick_x) >0.25) {
+            double vector_Add =  (-1 * gamepad1.left_stick_y) + gamepad1.left_stick_x;
+            double vector_Sub =  (-1 * gamepad1.left_stick_y) - gamepad1.left_stick_x;
+            int negative_Add = vector_Add<0?-1:1;
+            int negative_sub = vector_Sub<0?-1:1;
+            double f_Vector_add = Math.abs(vector_Add)>1? negative_Add : vector_Add;
+            double f_Vector_sub = Math.abs(vector_Sub)>1? negative_sub : vector_Sub;
+
+
+            frontLeft.setPower(f_Vector_add);
+            frontRight.setPower(f_Vector_sub);
+            backLeft.setPower(f_Vector_sub);
+            backRight.setPower(f_Vector_add);
+
+            if(gamepad1.right_stick_x > 0.25)
+            {
+                //turn clock wise
+                frontLeft.setPower(1);
+                frontRight.setPower(-1);
+                backLeft.setPower(1);
+                backRight.setPower(-1);
             }
-            else{
-                frontLeft.setPower(0);
-                frontRight.setPower(0);
-                backLeft.setPower(0);
-                backRight.setPower(0);
+            if(gamepad1.right_stick_x < -0.25)
+            {
+                //turn counter clock wise
+                frontLeft.setPower(-1);
+                frontRight.setPower(1);
+                backLeft.setPower(-1);
+                backRight.setPower(1);
             }
+        }
+        else{
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
         }
     }
 }
