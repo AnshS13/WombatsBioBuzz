5977cfb5cbf6858944b55db9cc1dd79795f2fcd8
APSCoding13 <51973395+AnshS13@users.noreply.github.com>
2026-09-17 17:56:25 -0700

    message from remote to local?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPCode.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 //This is a test comment in the local file
+//Now I am dangerously changing in the remote.
 
 @TeleOp(name = "Testing2")
 public class TeleOPCode extends LinearOpMode {
